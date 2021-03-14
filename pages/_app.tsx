d310a612d09eba7736098493924574acb51a715c
Alisa <alisa@ganguo.hk>
2021-03-14 22:14:43 +0800

    build: Construct a new next.js with typescript project.

---
+++ pages/_app.tsx
@@ -0,0 +1,8 @@
+import '../assets/styles/app.scss';
+
+// eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
+function MyApp({ Component, pageProps }:any):any {
+    return <Component {...pageProps} />;
+}
+
+export default MyApp;
