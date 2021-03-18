2e678e4c04d8a743acf92be0319d49cc48bd7cff
AlisaYang <yang.jingyan@icloud.com>
2021-03-18 08:59:37 +0000

    Deploying to develop from  @ d631de37f68a50d033caaa893b9194a4c28d85ca 🚀

--- pages/_app.tsx
+++
@@ -1,8 +0,0 @@
-import '../assets/styles/app.scss';
-
-// eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
-function MyApp({ Component, pageProps }:any):any {
-    return <Component {...pageProps} />;
-}
-
-export default MyApp;
